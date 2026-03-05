c7f14830638105824402c532d4636c49b6c15566
Anthony Le <theant741@gmail.com>
2026-03-05 18:24:15 -0500

    added metadata and collections data for collections photos

---
+++ dejavu/src/data/collectionsMeta.js
@@ -0,0 +1,8 @@
+export const COLLECTIONS_META = [
+    { id: '012', title: '012/Spring-Summer 2026', folder: '012' },
+    { id: '011', title: '011/Autumn-Winter 2025', folder: '011' },
+    { id: '010', title: '010/Spring-Summer 2025', folder: '010' },
+    { id: '009', title: '009/Autumn-Winter 2024', folder: '009' },
+    { id: '008', title: '008/Spring-Summer 2024', folder: '008' },
+    { id: '007', title: '007/Autumn-Winter 2023', folder: '007' }
+];
