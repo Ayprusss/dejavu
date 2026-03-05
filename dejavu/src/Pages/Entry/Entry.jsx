97170860258e21e6a7ebc462add577cab2a3f279
Anthony Le <theant741@gmail.com>
2026-03-05 18:23:46 -0500

    added links to shop through image and collection link

--- dejavu/src/Pages/Entry/Entry.jsx
+++ dejavu/src/Pages/Entry/Entry.jsx
@@ -1,20 +1,21 @@
+import { Link } from 'react-router-dom';
 import './Entry.css';
 import introPhoto from './dejavu-intro-photo.webp';
 
 function Entry() {
   return (
     <section className="entry-page" aria-label="Deja Vu entry">
-      <div className="entry-image-wrap">
+      <Link className="entry-image-wrap" to="/shop">
         <img
           className="entry-image"
           src={introPhoto}
           alt="Deja Vu SS26 model in lookbook styling"
         />
-      </div>
+      </Link>
 
-      <a className="entry-collection-link" href="#">
+      <Link className="entry-collection-link" to="/shop">
         SS26 2nd Delivery
-      </a>
+      </Link>
     </section>
   );
 }
