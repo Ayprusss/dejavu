2a46b537f3167b10df1eef28bfe197afaa62a676
Anthony Le <theant741@gmail.com>
2026-03-13 17:45:43 -0400

    added controller files for admin features, authentification and product data handling

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -55,6 +55,52 @@ const register = async (req, res) => {
     }
 }
 
+const login = async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(400).json({ message: "Email and password are required" });
+    }
+
+    try {
+        const { data: user, error } = await supabase.from("User").select("*").eq("email", email).single();
+
+        if (error && error.code === 'PGRST116') {
+            return res.status(401).json({ message: "Invalid email or password" });
+        } else if (error) {
+            throw error;
+        }
+
+        const isMatch = await bcrypt.compare(password, user.passwordHash);
+
+        if (!isMatch) {
+            return res.status(401).json({ message: "Invalid email or password" });
+        }
+
+        const token = jwt.sign(
+            { id: user.id, isAdmin: user.isAdmin },
+            process.env.JWT_SECRET || 'super_secret_jwt_key',
+            { expiresIn: "24h" }
+        );
+
+        res.status(200).json({ 
+            message: "Login successful", 
+            user: {
+                id: user.id,
+                email: user.email,
+                firstName: user.firstName,
+                lastName: user.lastName,
+                isAdmin: user.isAdmin
+            }, 
+            token 
+        });
+    } catch (error) {
+        console.error("Error thrown when logging in user: ", error);
+        return res.status(500).json({ message: "Internal server error" });
+    }
+}
+
 module.exports = {
-    register
+    register,
+    login
 };
