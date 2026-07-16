39104863ecb5443d246b3b764d51351802d73f90
Anthony Le <theant741@gmail.com>
2026-07-15 20:25:33 -0400

    created index data constant

---
+++ dejavu/src/data/indexData.js
@@ -0,0 +1,22 @@
+export const INDEX_IMAGES = [
+  {
+    id: "contrast-stitch",
+    label: "Contrast Stitch Longsleeve Top + Grisaille Belt",
+    src: "/Index/DSC5535_SnapseedCopy.webp"
+  },
+  {
+    id: "everyday-bag",
+    label: "Everyday bag",
+    src: "/Index/everydayBag.webp"
+  },
+  {
+    id: "special-label",
+    label: "Special Label",
+    src: "/Index/Special_Label.webp"
+  },
+  {
+    id: "aw25-backstage",
+    label: "AW25 Backstage shot of james and mohammed",
+    src: "/Index/AW25_Backstage_shot_of_james_and_mohammed.webp"
+  }
+];
